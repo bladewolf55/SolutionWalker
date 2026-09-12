e908aa6adc36cffa4b698eb4fea5964f89d9fc3f
Charles Flatt <charles@tfhome.us>
2026-09-12 11:05:48 -0600

    Add code comment on filtered collection properties

--- SolutionWalker/Domain/Models/Project.cs
+++ SolutionWalker/Domain/Models/Project.cs
@@ -5,7 +5,19 @@ public record Project : ProjectProperties
     public virtual List<Solution> Solutions { get; set; } = null!;
     public virtual List<Reference> References { get; set; } = null!;
 
+    /// <summary>
+    /// ONLY available client side so must <c>ToList()</c> first, e.g.
+    /// <c>var x = db.Projects.ToList().Where(p => p.ProjectReferences.Any());</c>
+    /// </summary>
     public List<Reference> ProjectReferences => References.Where(a => a.ReferenceType == "Project").ToList();
+    /// <summary>
+    /// ONLY available client side so must <c>ToList()</c> first, e.g.
+    /// <c>var x = db.Projects.ToList().Where(p => p.ProjectReferences.Any());</c>
+    /// </summary>
     public List<Reference> PackageReferences => References.Where(a => a.ReferenceType == "Package").ToList();
+    /// <summary>
+    /// ONLY available client side so must <c>ToList()</c> first, e.g.
+    /// <c>var x = db.Projects.ToList().Where(p => p.ProjectReferences.Any());</c>
+    /// </summary>
     public List<Reference> SystemReferences => References.Where(a => a.ReferenceType == "System").ToList();
 }
